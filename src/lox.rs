fd35ec0daf80a7d6ec3023dda06a2e25346dc077
Gurwi <gs3335769@gmail.com>
2025-06-22 16:24:16 +0200

    Testing

--- src/lox.rs
+++ src/lox.rs
@@ -1,16 +1,16 @@
 use crate::tokenizer::{Token, TokenType};
 
 pub fn report(line: usize, where_msg: &str, message: &str) {
-    eprintln!("[line {line}] Error {where_msg}: {message}");
+    eprintln!("[line {line}] Error{where_msg}: {message}");
 }
 
 pub fn error(token: &Token, message: &str) {
     match token.token_type {
         TokenType::EOF => {
-            report(token.line, "at end", message);
+            report(token.line, " at end", message);
         }
         _ => {
-            report(token.line, &format!("at '{}'", token.lexeme), message);
+            report(token.line, &format!(" at '{}'", token.lexeme), message);
         }
     }
 }
